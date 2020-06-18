1c987e6fc5bafec80e51e8db628a808ae2995032
Steve Zhang <sz.stevezhang.sz@gmail.com>
2020-06-17 19:58:48 -0700

    new file:   .DS_Store 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	modified:   circularQueue.hpp 	new file:   main 	modified:   main.cpp 	new file:   main.dSYM/Contents/Info.plist 	new file:   main.dSYM/Contents/Resources/DWARF/main 	new file:   randomStuff.txt

--- circularQueue.hpp
+++ circularQueue.hpp
@@ -1,27 +1,54 @@
+#ifndef CIRCULARQUEUEHPP
+#define CIRCULARQUEUEHPP
 #include <cstddef>
-template<class T ,std::size_t N /*Max capacity*/>
+#include <type_traits>
+
+template<class T ,std::size_t N /*Max capacity*/, 
+typename Idxtype = std::size_t /*Integral type to store indices. May change,
+like to uint_least16_t, to lower space usage*/,
+typename = typename std::enable_if<std::is_integral<Idxtype>::value>::type>
 class circularQueue{
     union myUnion{
         bool forConstexprCtor;
         T value;
-        constexpr myUnion() : forConstexprCtor{true} {};
+        constexpr myUnion() : forConstexprCtor{true} {}
+
+        template<class PossibleUnion,typename = typename std::enable_if<std::is_same<PossibleUnion, myUnion>::value >::type>
+        constexpr myUnion(PossibleUnion&& other) : value{other.value} {}
+
+        template<typename... Args,typename = typename std::enable_if<std::is_constructible_v<T,Args>...>::type> 
+        constexpr myUnion(Args&&... args) : value(std::forward<Args>(args)...) {}
+
+        template<typename AnotherUnion>
+        constexpr void operator=(const AnotherUnion&& other){
+            value = other.value;
+        }
     };
-    myUnion theArray[N];
+    struct myStruct{
+        myUnion theArray[N];
+        template<typename... t>
+        constexpr myStruct(t&&... theList) : theArray{(theList)...} {}
+    } mS;
     //Head == idx of element at the front. Tail == idx of last element + 1. theSize == queue's size
-    std::size_t head, tail, theSize;
+    Idxtype head, tail, theSize;
+    
     public:
         constexpr circularQueue() : head{0}, tail{0}, theSize{0} {}
+        explicit constexpr circularQueue(const circularQueue<T,N>& other) : mS{other.mS}, head{other.head}, 
+        tail{other.tail}, theSize{other.theSize} {}
+        
+        explicit constexpr circularQueue(circularQueue<T,N>& other) : 
+        circularQueue{const_cast<circularQueue<T,N> const&>(other)} {}
+        
+        template<typename... Args/*, typename = typename std::enable_if<std::is_constructible_v<myStruct, Args>... >::type*/ >
+        explicit constexpr circularQueue(Args&&... theList) : mS{(theList)...}, head{0},
+        tail{sizeof...(theList)}, theSize{sizeof...(theList)}{}
     
-        template<typename... t>
-        constexpr circularQueue(t&&... theList) : head{0}, tail{0}, theSize{sizeof...(theList)}{
-            static_assert(sizeof...(theList) <= N, "The number of elements must be less than or equal to N\n");
-            theArray[tail++].value = (T(theList),...);
-        }
-        constexpr bool push(T theObj){
+        constexpr bool push(const T theObj){
             if(theSize == N){
                 return false;//queue is full
             }
-            theArray[(tail == N ? tail = 0 : tail++)].value = theObj;
+            mS.theArray[(tail == N ? (tail = 0)++ : tail++)] = myUnion(std::move(theObj));
             return ++theSize; //++theSize always > 0. Return true
         }
         template<typename ...Args> 
@@ -29,12 +56,12 @@ class circularQueue{
             if(theSize == N){
                 return false;//queue is full
             }
-            theArray[(tail == N ? tail = 0 : tail++)] = T(std::forward<Args>(args)...);
+            mS.theArray[(tail == N ? (tail = 0)++ : tail++)] = myUnion(std::forward<Args>(args)...);
             return ++theSize;
         }
 
         constexpr const T& front() const noexcept{
-            return theArray[head].value;
+            return mS.theArray[head].value;
         }
 
         constexpr bool pop() noexcept{
@@ -46,10 +73,46 @@ class circularQueue{
         constexpr bool empty() const noexcept{
             return !theSize;
         }
-        constexpr std::size_t size() const noexcept{
+        constexpr Idxtype size() const noexcept{
             return theSize;
         }
         constexpr std::size_t maxCapacity() const noexcept{
             return N;
         }
+        //Assignment
+        constexpr circularQueue& operator=(const circularQueue<T,N>& other){ 
+            std::size_t first{head = other.head};
+            tail = other.tail;
+            theSize = other.theSize;
+            if(other.tail < other.head){ //Only need to copy elements from other.head to other.tail
+                for(; first < N; ++first){
+                    mS.theArray[first] = other.mS.theArray[first];
+                }
+                for(first = 0; first < tail; ++first){
+                    mS.theArray[first] = other.mS.theArray[first];
+                }
+            }
+            else{
+                for(; first < other.tail; ++first) mS.theArray[first] = other.mS.theArray[first];
+            }
+            return *this;
+        }
+        constexpr circularQueue& operator=(const circularQueue<T,N>&& other){ 
+            std::size_t first{head = std::move(other.head)};
+            tail = std::move(other.tail);
+            theSize = std::move(other.theSize);
+            if(other.tail < other.head){ //Only need to copy elements from other.head to other.tail
+                for(; first < N; ++first){
+                    mS.theArray[first] = std::move(other.mS.theArray[first]);
+                }
+                for(first = 0; first < tail; ++first){
+                    mS.theArray[first] = std::move(other.mS.theArray[first]);
+                }
+            }
+            else{
+                for(; first < other.tail; ++first) mS.theArray[first] = std::move(other.mS.theArray[first]);
+            }
+            return *this;
+        }
 };
+#endif //CIRCULARQUEUEHPP
--- main.cpp
+++ main.cpp
@@ -1,32 +1,64 @@
 #include <iostream> //Print statements for debugging
 #include "circularQueue.hpp"
+
 void testingCircularQueue(){
-    circularQueue<std::pair<uint_least16_t,uint_least16_t>, 3> cQ(
-            std::pair{59991,2}, std::pair{3,4} );
+    circularQueue<std::pair<uint_least16_t,uint_least16_t>, 3> cQ(std::pair{59991,2}/*, std::pair{3,4}*/ );
+    std::cout << std::boolalpha << cQ.push({3,3}) << ' ' << cQ.push({13,13}) << ' '
+    << cQ.push({32,23}) << ' ' << cQ.push({321,231}) << ' ' << cQ.push({321,231})
+    << ' ' << cQ.push({321,231}) << '\n';
 
-        std::cout << std::boolalpha << cQ.push({3,3}) << ' ' << cQ.push({13,13}) << ' '
-        << cQ.push({32,23}) << ' ' << cQ.push({321,231}) << ' ' << cQ.push({321,231})
-        << ' ' << cQ.push({321,231}) << '\n';
+    cQ.pop();
+    std::cout << cQ.push({3,3}) << ' ' << cQ.push({321,231}) << '\n';
+    std::cout << "Begin popping\n";
+    std::cout << cQ.pop() << ' ' << cQ.pop() << ' ' << cQ.pop() << ' ' << cQ.pop() << '\n';
 
-        cQ.pop();
-        std::cout << cQ.push({3,3}) << ' ' << cQ.push({321,231}) << '\n';
-        std::cout << "Begin popping\n";
-        std::cout << cQ.pop() << ' ' << cQ.pop() << ' ' << cQ.pop() << ' ' << cQ.pop() << '\n';
+    std::cout << "Initially empty queue\n";
+    circularQueue<char, 5> charCq;
+    std::cout << charCq.pop() << '\n';
+    charCq.push('D');
+    std::cout << charCq.front() << ' ' << charCq.pop() << '\n';
 
-        std::cout << "Initially empty queue\n";
-        circularQueue<char, 5> charCq;
-        std::cout << charCq.pop() << '\n';
-        charCq.push('D');
-        std::cout << charCq.front() << ' ' << charCq.pop() << '\n';
+    circularQueue<char, 2> newQueue;
+    std::cout <<  newQueue.push('a') << ' ' 
+    << newQueue.push('b') << ' ' 
+    << newQueue.push('c') << ' '
+    << newQueue.pop() << ' ' << newQueue.push('Z') << '\n';
 
-        circularQueue<char, 2> newQueue;
-        std::cout <<  newQueue.push('a') << ' ' << newQueue.push('b') << ' ' << newQueue.push('c') << ' '
-        << newQueue.pop() << ' ' << newQueue.push('c') << '\n';
+    const circularQueue<char, 2> copy(newQueue); //test copy with non const obj
+    const circularQueue<char, 2> copyOfcopy(copy);//test copy with const obj
+    [[maybe_unused]] constexpr circularQueue<char, 2> fromTemp((circularQueue<char, 2>())); //from temp
+    circularQueue<char, 2> testAssignment = circularQueue<char, 2>();//assignment with r value
+    testAssignment = copy; //assignment with const copy
+    testAssignment = newQueue;//assignment with non const copy
+    constexpr circularQueue<char, 2, uint_least8_t> notAnError('A', 'Z');
+    [[maybe_unused]] constexpr circularQueue<char, 2, uint_least8_t> copyFromConstexpr(notAnError);
+    std::cout << sizeof(copy) << ' ' << sizeof(notAnError) << '\n';
+    struct A{};
+    //[[maybe_unused]] constexpr circularQueue<char, 2, A> errorThisis;
 
-        
+    
+    for(circularQueue<char, 5, uint_least8_t> testingFrontAndPop{'A', 'B', 'C', 'D', 'E'}; 
+    !testingFrontAndPop.empty(); testingFrontAndPop.pop()){
+        std::cout << testingFrontAndPop.front() << ' ';
+    }
+    std::cout << '\n';
+}
+constexpr bool otherTest(){
+    circularQueue<char, 2> newQueue;
+    constexpr char anA{'a'};
+    newQueue.push(anA);
+    newQueue.push('b');
+    newQueue.push('c');
+    newQueue.pop();
+    newQueue.emplace('d');
+    newQueue.pop();
+    return newQueue.emplace('e');
 }
 
 int main(){
 testingCircularQueue();
+if constexpr(otherTest()) std::cout << "If constexpr succeeded\n";
+//std::cout << std::is_integral<std::string>::value << '\n';
+//std::cout << std::boolalpha << std::is_constructible<std::pair<uint_least16_t,uint_least16_t>,std::pair<uint_least16_t,uint_least16_t>>::value << '\n';
 }
 //clang++ -Wall -std=c++1z -stdlib=libc++ -g main.cpp -o main && ./main
